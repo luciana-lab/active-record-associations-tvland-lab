1fb45c1d1b1b076d2397e8a52d7102dcf0ab9b87
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-04 13:00:00 -0400

    commit tree 15bc7d0942e424c77e58c69d6f231ce9f5621f63

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    character = self.characters.map {|name| name.name}
+    show = self.shows.map {|name| name.name}
+    list_roles = character + show
+    list_roles.split(",").join(" - ")
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,21 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    array = []
+    show_id = self.characters.map {|show| show.show_id}
+    actor_id = self.characters.map {|actor| actor.actor_id}
+    array << self.id
+    
+    if array == show_id && self.actors.ids == actor_id
+        list = []
+        first_name = self.actors.map {|name| name.first_name}
+        last_name = self.actors.map {|name| name.last_name}
+        name = first_name + last_name
+        list << name.split(",").join(" ")
+  end
+  end
+
 end
